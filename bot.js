6b2f88169a2e638d64dd659690bcbe62ff2570e8
eyok <yd69655@uga.edu>
2016-12-13 16:17:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,22 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/bot$/;
+      botRegex = /^\/bot$/,
+      botRegey = /^\/link$/;
+  
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  if(request.text && botRegey.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage2();
     this.res.end();
   } else {
     console.log("don't care");
@@ -53,5 +64,39 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function postMessage2() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "http://www.ordercosmic.com/admin?verifyMe=rff8e7f5f5w";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
