20fd673358d3653d650f583d4e8c7797dd0e0799
eyok <yd69655@uga.edu>
2016-12-13 13:43:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
-var requestGlobal;
+
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/bot$/;
-  requestGlobal = request;
+      botRegex = /^\/cool guy$/;
+
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
@@ -18,11 +18,10 @@ function respond() {
   }
 }
 
-
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = requestGlobal;//cool();
+  botResponse = "hello world";//cool();
 
   options = {
     hostname: 'api.groupme.com',
