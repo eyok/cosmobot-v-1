0779a93275a2e1f3dc26dd0bf8aa191d51846bdf
eyok <yd69655@uga.edu>
2016-12-13 13:38:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
-var request = JSON.parse(this.req.chunks[0])
+var request = JSON.parse(this.req.chunks[0]);
 var botID = process.env.BOT_ID;
 
 function respond() {
