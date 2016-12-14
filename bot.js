c26e00cd4db6d262bf684d9fc2915c1199ff2bc1
eyok <yd69655@uga.edu>
2016-12-13 23:17:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,7 +96,7 @@ function postMessage() {
 function postMessage2() {
   var botResponse, options, body, botReq;
 
-  botResponse = "http://www.ordercosmic.com/admin?verifyMe=rff8e7f5f5w";
+  botResponse = "http://www.ordercosmic.com/admin?verifyMe=39Efkeo93j";
 
   options = {
     hostname: 'api.groupme.com',
