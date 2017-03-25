0db788906d3a1ca89cb57db76de8bb9ca2770c1e
eyok <yd69655@uga.edu>
2017-03-25 09:12:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ function respond() {
       botRegey = /^\/link$/,
       botRegez = /^\/sign$/,
       botRegea = /^\/bot$/,
-      botRegeb = /^\/reserve$/;
-      botRegec = /^\/nedza$/
+      botRegeb = /^\/reserve$/,
+      botRegec = /^\/nedza$/;
   
   if(request.text && botRegeb.test(request.text)) {
     this.res.writeHead(200);
@@ -68,42 +68,7 @@ function respond() {
   }
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
-
-  botResponse = cool();
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-} //meme
-
-function postMessage2(s) {
+function postMessage(s) {
   var botResponse, options, body, botReq;
 
   botResponse = s;
@@ -136,6 +101,7 @@ function postMessage2(s) {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
-}
+} //meme
+
 
 exports.respond = respond;
